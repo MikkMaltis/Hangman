4e1867a673173e3bbe1056cef60112f7c96352dc
MikkMaltis <maltismikk@gmail.com>
2024-01-15 09:11:44 +0200

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÕÄÖÜ'.split('');
 const word = 'AMETIKOOL'.split('');
-
 let guess = [...Array(word.length).keys()].map(i => '_');
 
 const wordDiv = document.getElementById('word');
+const alphabetDiv = document.getElementById('alphabet');
 
 for ( let i in guess ) {
     const letterDiv = document.createElement('div');
@@ -12,15 +12,23 @@ for ( let i in guess ) {
     wordDiv.appendChild(letterDiv);
 }
 
+for ( let i in alphabet ) {
+    const aDiv = document.createElement('div');
+    aDiv.setAttribute = ('id', alphabet[i]);
+    aDiv.innerText = alphabet[i];
+    wordDiv.appendChild(aDiv);
+}
+
 document.addEventListener('keyup', e => {
-    if ( alphabet.includes(e.key.toUpperCase()) ) {
+    const g = e.key.toUpperCase();
+    if ( alphabet.includes(g) ) {
         if ( word.includes(g) ) {
             for ( let i in word) {
                 if ( word[i] == g) {
                     const letterDiv = document.getElementById('letter_' + i);
+                    letterDiv.innerText = g;
                 }
             }
         }
-    console.log(e.key);
     }
 });
